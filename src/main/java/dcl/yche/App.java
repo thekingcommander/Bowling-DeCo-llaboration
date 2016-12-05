cd8a95edb084655929b3984a600000e4572cb09a
yaakoub <c.yak32000@gmail.com>
2016-12-05 05:07:57 +0100

    home commit

---
+++ src/main/java/dcl/yche/App.java
@@ -0,0 +1,13 @@
+package dcl.yche;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
